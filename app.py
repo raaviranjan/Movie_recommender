8cdd316eab7da6d21b0b89b6a47358c3988e216a
Ravi Ranjan <raviranjan091@gmail.com>
2020-12-05 13:37:37 +0530

    removed one import

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
